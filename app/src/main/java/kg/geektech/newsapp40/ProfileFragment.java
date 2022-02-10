a4820f5f2f2b9a6579eef276e8af1f2a0eedba3e
Kochevnikbbb <bbelekov@mail.ru>
2022-02-10 21:55:03 +0600

    Initial commit

---
+++ app/src/main/java/kg/geektech/newsapp40/ProfileFragment.java
@@ -0,0 +1,54 @@
+package kg.geektech.newsapp40;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.File;
+
+import kg.geektech.newsapp40.databinding.FragmentProfileBinding;
+
+
+public class ProfileFragment extends Fragment {
+    private FragmentProfileBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding=FragmentProfileBinding.inflate(LayoutInflater.from(requireContext()),
+                container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setImage();
+    }
+
+    private void setImage() {
+        ActivityResultLauncher<String> xzz = registerForActivityResult(new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri uri) {
+                    binding.ivImage.setImageURI(uri);
+                    }
+                });
+        binding.ivImage.setOnClickListener(view -> {
+          xzz.launch("image/*");
+
+        });
+    }
+}
